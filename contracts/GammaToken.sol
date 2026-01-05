abf6d1b9a3d504c1602318c9f9800e814cc025ea
arp123-456 <arpitsidhi123@gmail.com>
2026-01-06 02:06:28 +0530

    Add GammaToken contract

---
+++ contracts/GammaToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title GammaToken
+ * @notice ERC20 token for Gamma swap protocol
+ */
+contract GammaToken is ERC20, Ownable {
+    constructor() ERC20("Gamma Token", "GAMMA") Ownable(msg.sender) {
+        _mint(msg.sender, 10_000_000 * 10**18); // 10M tokens
+    }
+    
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+    
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
